6c8e757426beaade99999562168be5f71c9087a8
densel.esekon2@student.moringaschool.com <esekondensel@gmail.com>
2023-10-20 13:29:59 +0300

    Defined the baseUrl for the API

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
+// When the page is fully loaded, we begin here.
 document.addEventListener("DOMContentLoaded", () => {
-    const baseUrl = "http://localhost:3000"; // Define the base URL for the API.
-     // Log the API base URL to verify.
 
-
+    // First, we define the base URL for the API.
+    const baseUrl = "http://localhost:3000";
+    console.log("API base URL:", baseUrl);
 });
-    
